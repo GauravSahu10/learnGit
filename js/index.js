90bce7f458fecf0eb98226bbd1082b7f1d35ea69
Gaurav <123gauravsahu@gmail.com>
2026-05-10 14:24:18 +0530

    learn git changes related js and html file

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+for (let i=0; i<9;i++)
+{
+    // console.log("i = ",i);
+}
+let j = 0;
+while (j<10) {
+    // console.log("j = ",j);
+    // j++;
+}
+
+do {
+    console.log("j = ",j);
+    j++;
+} while (j<10);
